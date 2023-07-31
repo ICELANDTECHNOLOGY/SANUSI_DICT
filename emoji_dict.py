0d73a5522e5ef454ded785fe610b092842010e6d
ICELANDTECHNOLOGY <139781937+ICELANDTECHNOLOGY@users.noreply.github.com>
2023-07-31 17:43:40 +0800

    Add files via upload

---
+++ emoji_dict.py
@@ -0,0 +1,15 @@
+__name__ = "__main__"
+
+
+
+emoji_dict = {
+    "Smile" : "😊",
+    "Happy" : "😁",
+    "Love" : "💖",
+    "sad" : "😒",
+    "crying" : "🤣",
+    "kiss" : "💋",
+    "correct" : "✔",
+    "Tired" : "🤦‍♂️",
+} 
+print(emoji_dict["correct"])
